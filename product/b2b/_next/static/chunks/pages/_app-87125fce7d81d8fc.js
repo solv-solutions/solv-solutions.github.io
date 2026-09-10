77435f463d8145288d932a4229099e37ae81fb61
edisigci <53569118+edisigci@users.noreply.github.com>
2026-09-10 18:45:15 +0300

    Deploy all (website@859891e)

---
+++ product/b2b/_next/static/chunks/pages/_app-87125fce7d81d8fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{26590:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(53935)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[6593,8792],()=>(n(26590),n(40567))),_N_E=_.O()}]);
